51f73e63cd55ac5cf18bf16eadf892a9b55050e1
Lesedi <motloungkgotso626@gmail.com>
2025-01-28 11:31:44 +0200

    Removed all the fleshy items, background pictures and simplified the site

---
+++ About.js
@@ -0,0 +1,42 @@
+const Card = document.getElementsByClassName("Textspace")[0];
+
+Card.addEventListener("mousemove", (event) => {
+    const pointerX = event.clientX;
+    const pointerY = event.clientY;
+    const cardRec = Card.getBoundingClientRect();
+    
+    const halfWidth = cardRec.width / 2;
+    const halfHeight = cardRec.height / 2;
+    
+    const centerX = cardRec.left + halfWidth;
+    const centerY = cardRec.top + halfHeight;
+    
+    const deltaX = centerX - pointerX;
+    const deltaY = centerY - pointerY;
+    
+    const rotateX = deltaY / halfHeight;
+    const rotateY = deltaX / halfWidth;
+    
+    const distanceFromCenter = Math.sqrt(Math.pow(deltaX, 2) + Math.pow(deltaY, 2));
+    const maxDistance = Math.max(halfWidth, halfHeight);
+    const rotateDegree = (distanceFromCenter * 10) / maxDistance;
+    
+    Card.style.transform = `perspective(600px) rotate3d(${rotateX}, ${-rotateY}, 0, ${rotateDegree}deg)`;
+});
+
+Card.addEventListener("mouseleave", () => {
+    Card.style.transform = '';
+});
+
+const prev = document.querySelector('.Controls#Prev span');
+const next = document.querySelector('.Controls#Next span');
+
+next.addEventListener('click', function() {
+    const items = document.querySelectorAll('.OurTeam .TeamCard');
+    document.querySelector('.OurTeam').appendChild(items[0]);
+});
+
+prev.addEventListener('click', function() {
+    const items = document.querySelectorAll('.OurTeam .TeamCard');
+    document.querySelector('.OurTeam').prepend(items[items.length - 1]);
+});
--- BrainFoods.js
+++ BrainFoods.js
@@ -1,97 +1,100 @@
-
-const product = [
-{
-	id:0,
-	image: 'Coke.jpeg',
-	title: "Coca cola",
-	price: 120,
-},
-{
-	
-	id:1,
-	image:  'Coke.jpeg',
-	title: " Coca cola",
-	price: 100,
-},
-{
-	
-	id:2,
-	image: 'Coke.jpeg',
-	title: " Coca cola",
-	price: 100,
-},
-{
-	
-	id:3,
-	image:  'Coke.jpeg',
-	title: " Coca cola",
-	price: 100,
-},
+const drinks = [
+    { id: 0, image: 'Coke.jpeg', title: "Coca cola", price: 120 },
+    { id: 1, image: 'Coke.jpeg', title: "Coca cola", price: 100 },
+    { id: 2, image: 'Coke.jpeg', title: "Coca cola", price: 100 },
+    { id: 3, image: 'Coke.jpeg', title: "Coca cola", price: 100 }
 ];
 
-const categories = [...new Set(product.map((item)=>{return item}))];
-	
-	let i=0;
-    document.getElementById('root').innerHTML = categories.map((item)=>
 
-{
-	var {image, title, price} = item;
-	return(
-		  ` <div class="box">
-		     <div class="imgBox">
-		        <img class="images" src=${image}></img>
-			</div>
-		 <div class="bottom">
-		<p>${title}</p>
-		<h2>$ ${price}.00</h2>`+ "<button onclick='addtobasket("+(i++)+")'>Add to Basket</button>"+ `</div>
-		</div>`
-		
-		)
+const snacks = [
+    { id: 0, image: 'Coke.jpeg', title: "Coca cola", price: 120 },
+    { id: 1, image: 'Coke.jpeg', title: "Coca cola", price: 100 },
+    { id: 2, image: 'Coke.jpeg', title: "Coca cola", price: 100 },
+    { id: 3, image: 'Coke.jpeg', title: "Coca cola", price: 100 }
+];
+
+
+const sweets = [
+    { id: 0, image: 'Coke.jpeg', title: "Coca cola", price: 120 },
+    { id: 1, image: 'Coke.jpeg', title: "Coca cola", price: 100 },
+    { id: 2, image: 'Coke.jpeg', title: "Coca cola", price: 100 },
+    { id: 3, image: 'Coke.jpeg', title: "Coca cola", price: 100 }
+];
+
+
+
+let i = 0;
+
+const renderItems = (items, category) => {
+    document.getElementById(category).innerHTML = items.map((item) => {
+        const { image, title, price } = item;
+        return (
+            `<div class="box">
+                <div class="imgBox">
+                    <img class="images" src=${image} alt="${title}">
+                </div>
+                <div class="bottom">
+                    <p>${title}</p>
+                    <h2>$${price}.00</h2>` +
+            `<button onclick='addtobasket(${item.id}, "${category}")'>Add to Basket</button>` +
+            `</div>
+            </div>`
+        );
+    }).join('');
+   
+};
+
+let basket = [];
+
+function addtobasket(id, category) {
+    let item;
+    if (category === 'Drinks') {
+        item = drinks.find(drink => drink.id === id);
+    } else if (category === 'Snacks') {
+        item = snacks.find(snack => snack.id === id);
+    } else if (category === 'Sweets') {
+        item = sweets.find(sweet => sweet.id === id);
+    }
+    basket.push({item});
+    displayBasket();
 }
-).join('');
 
-var basket =[];
+function delElement(index) {
+    basket.splice(index, 1);
+    displayBasket();
+}
 
-function addtobasket(a)
-{
-	basket.push({...categories[a]});
-	displayBasket();
+function displayBasket() {
+    let j = 0, total = 0;
+    document.getElementById('Itemcount').innerHTML = basket.length;
+    document.getElementById('total').innerHTML = "$0.00";
+    let data = "<p id='basket-text'>Basket is empty, buy to power up the brain...</p>";
+
+    if (basket.length === 0) {
+        document.getElementById('cartItem').innerHTML = data;
+    } else {
+        document.getElementById("cartItem").innerHTML = basket.map((item) => {
+            const { image, title, price } = item;
+            total += price;
+            document.getElementById("total").innerHTML = "$" + total + ".00";
+            return (
+                `<div class='cart-item'>
+                    <div class='row-img'>
+                        <img class='rowimg' src=${image} alt="${title}">
+                    </div>
+                    <p style='font-size:25px;'>${title}</p>
+                    <h2 style='font-size:20px;'>$${price}.00</h2>` +
+                `<button class='trash' onclick='delElement(${j++})'>Remove</button></div>`
+            );
+        }).join('');
+    }
 }
 
 
-function delElement(a)
-{
-	basket.splice(a,1);
-	displayBasket();
-}
+renderItems(drinks, 'Drinks');
+renderItems(snacks, 'Snacks');
+renderItems(sweets, 'Sweets');
 
-function displayBasket(a)
-{
-	let j =0,total=0;
-	document.getElementById('Itemcount').innerHTML=basket.length;
-	document.getElementById('total').innerHTML = "$ "+0+".00";
-	var data =  "<p id='basket-text'>Basket is empty, buy to power up the brain...</p>" ;
-	
-	if(basket.length==0)
-	{
-		document.getElementById('cartItem').innerHTML = data;
-	}
-	else
-	{
-		document.getElementById("cartItem").innerHTML = basket.map((items)=>
-		{
-			var {image,title,price} = items;
-		    total = total + price;
-			document.getElementById("total").innerHTML = "$ "+ total +".00";
-			return(
-				`<div class='cart-item'>
-				 <div class='row-img'>
-				     <img class='rowimg' src=${image}>
-				 </div>
-					 <p style='font-size:25px;'>${title}</p>
-					 <h2 style='font-size:20px;'>$ ${price}.00</h2>`
-					 +"<button class='trash' onclick='delElement("+(j++) +")'>Remove</button></div>"
-				  );
-		}).join('');
-	}
-}
+let Product = document.getElementByClassName("cart-item");
+// Product.style.box-shadow = "20px 20px 50px rgba(0,0,0,.5)";
+
